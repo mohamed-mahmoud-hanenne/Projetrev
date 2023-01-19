e9b585383e3f8c672feb3ff1744b77b4d68b6fbe
mohamed mahmoud hanenne <medhanenne38@gmail.com>
2023-01-19 16:22:52 +0100

    comp

---
+++ commerce.js
@@ -0,0 +1,11 @@
+let searchprd = document.querySelector('.search-prd');
+
+document.querySelector('#sharp').onclick = () =>{
+    searchprd.classList.toggle('active');
+}
+
+let login = document.querySelector('.login');
+
+document.querySelector('#user').onclick = () =>{
+    login.classList.toggle('active');
+}
