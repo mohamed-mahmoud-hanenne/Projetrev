2daa15d465f0b3e4e49554d23829a8f5374416fc
mohamed mahmoud hanenne <medhanenne38@gmail.com>
2023-03-22 11:55:50 +0000

    cmt

--- commerce.js
+++ commerce.js
@@ -9,3 +9,15 @@ let login = document.querySelector('.login');
 document.querySelector('#user').onclick = () =>{
     login.classList.toggle('active');
 }
+
+let pr = document.querySelector(".a2");
+
+pr.onclick = function() {
+    location.href = "/#Pr";
+}
+
+let shpg = document.querySelector('.shop');
+
+document.querySelector('#shp').onclick = () =>{
+    shpg.classList.toggle('active');
+}
